677118481adcdc5c48a8c9541bb901977910c445
thisismykingddomcum@gmail.com <kvassoedik@gmail.com>
2025-05-12 23:11:20 +0300

    images added

--- frontend/src/components/MenuItem.jsx
+++ frontend/src/components/MenuItem.jsx
@@ -5,7 +5,7 @@ const MenuItem = ({id, name, price, img, onAddToCart}) => {
 
     return (
         <div className="menu-item">
-            <img source={img} alt={name + " image"} className="menu-item-img"/>
+            <img src={img} alt={name + " image"} className="menu-item-img"/>
             <p className="menu-item-name">{name}</p>
             <button onClick={() => onAddToCart(id)} className="menu-item-price">${price}</button>
         </div>
