193726e27c9704d6c9ff43477900975c06589c49
GEEG <e.glebov@innopolis.university>
2025-05-13 16:59:42 +0300

    Minor fix + increase code clarity for cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,45 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
-// function placeOrder(){
-//     const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
-//     const token = localStorage.getItem('access_token');
-//     const aboba = localStorage.getItem('cart');
-//     const navigate = useNavigate();
-//     const location = useLocation();
-//     const queryParams = new URLSearchParams(location.search);
-//     const table_id = queryParams.get('table');
-//     const restaurant_id = queryParams.get('restaurant');
-//     console.log(aboba)
-//     if (!token) {
-//         console.log('You must be logged in to place an order.');
-//     }
-
-//     fetch(`${API_BASE_URL}/api/order/`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({ table_id, restaurant_id, items: cart }),
-//     })
-//         .then((response) => {
-//             if (!response.ok) {
-//                 throw new Error('Failed to place order');
-//             }
-//             return response.json();
-//         })
-//         .then((data) => {
-//             console.log('Order placed successfully!');
-//             localStorage.setItem('order_id', data.id);
-//             setCart({}); // Clear cart
-//             localStorage.removeItem('cart'); // Remove cart from localStorage
-//             navigate(`/order/${data.id}`); // Redirect to order details page using order ID
-//         })
-//         .catch((error) => {
-//             console.error('Error:', error);
-//             alert('Failed to place order. Please try again.');
-//         });
-// }
 
 function Cart() {
     const navigate = useNavigate();
@@ -47,7 +8,7 @@ function Cart() {
     const queryParams = new URLSearchParams(location.search);
     const [table_id, setTable] = useState(queryParams.get('table'));
     const [restaurant_id, setRestaurant] = useState(queryParams.get('restaurant'));
-    
+
     const [cart, setCart] = useState(() => {
         const savedCart = localStorage.getItem('cart');
         return savedCart ? JSON.parse(savedCart) : [];
@@ -63,10 +24,12 @@ function Cart() {
             for (const [itemId] of Object.entries(cart)) {
                 try {
                     const response = await fetch(`${API_BASE_URL}/api/items/${itemId}`);
+                    // const response = fetch(`${API_BASE_URL}/api/items/${itemId}`);
                     if (!response.ok) {
                         throw new Error('Failed to fetch item details');
                     }
                     const data = await response.json();
+                    // const data =  response.json();
                     newItemsInfo[itemId] = data;
                 } catch (error) {
                     console.error(`Error fetching item ${itemId}:`, error);
@@ -78,21 +41,6 @@ function Cart() {
         fetchItemsInfo();
     }, [cart]);
 
-        useEffect(() => {
-        if (table_id) {
-            localStorage.setItem('table_id', table_id);
-        }
-        else {
-            setTable(localStorage.getItem('table_id'));
-        }
-        if (restaurant_id) {
-            localStorage.setItem('restaurant_id', restaurant_id);
-        }
-        else {
-            setRestaurant(localStorage.getItem('restaurant_id'));
-        }
-
-  }, [table_id, restaurant_id]);
     useEffect(() => {
         if (table_id) {
             localStorage.setItem('table_id', table_id);
@@ -107,16 +55,13 @@ function Cart() {
             setRestaurant(localStorage.getItem('restaurant_id'));
         }
 
-    }, [table_id, restaurant_id]);
+  }, [table_id, restaurant_id]);
+
     function placeOrder(){
         const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
-        const token = localStorage.getItem('access_token');
         const aboba = localStorage.getItem('cart');
-        console.log(aboba)
-        if (!token) {
-            console.log('You must be logged in to place an order.');
-        }
-    
+        console.log(aboba);
+        
         fetch(`${API_BASE_URL}/api/order/`, {
             method: 'POST',
             headers: {
@@ -143,9 +88,17 @@ function Cart() {
             });
     }
     var total = 0;
-    // Object.entries(cart).map(([itemId, item]) => {
-    //     const inf = itemsInfo[itemId];
-    //     total += inf.price * cart[itemId];});
+    Object.entries(cart).map(([itemId, item]) => {
+        const inf = itemsInfo[itemId];
+        console.log("Item ID:", itemId);
+        console.log("Item:", item);
+        console.log("Info:", inf);
+        if (inf) {
+            // total += inf.price * cart[itemId];
+            total += inf.price * item;
+        }
+        
+    });
 
     return (
         <div className='cart-main'>
